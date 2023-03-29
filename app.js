b54b11d9476e144a77fc789579579f0ce3c73841
Joseph M <jmedina1985@hotmail.com>
2023-03-28 17:14:28 -0700

    Working on the script

--- app.js
+++ app.js
@@ -3,4 +3,10 @@ gsap.registerPlugin(Flip);
 const links = document.querySelectorAll('.nav-item a');
 const activeNav = document.querySelector('.active-nav');
 
-links.forEach((link) => {});
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    gsap.to(links, { color: '#252525' });
+  });
+
+  if(document.activeElement)
+});
