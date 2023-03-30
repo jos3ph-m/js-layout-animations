4cdbea00bbb0d5a41be26a2482cbf18374415e0a
Joseph M <jmedina1985@hotmail.com>
2023-03-29 20:07:14 -0700

    Working on the script

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const links = document.querySelectorAll('.nav-item a');
 const activeNav = document.querySelector('.active-nav');
 
 links.forEach((link) => {
-  link.addEventListener('click', (e) => {});
+  link.addEventListener('click', (e) => {
+    gsap.to(links, { color: '#252525' });
+  });
 
   if (document.activeElement === e.target) {
     gsap.to(link, { color: '#385ae0' });
