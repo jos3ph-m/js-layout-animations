ad3f91561b0d908d54c6cab51f106b3624d14299
Joseph M <jmedina1985@hotmail.com>
2023-03-31 13:59:08 -0700

    Working on the script

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ links.forEach((link) => {
     const state = Flip.getState(activeNav);
     link.appendChild(activeNav);
     Flip.from(state, {
-      duration: 1.5,
+      duration: 1.2,
       absolute: true,
     });
   });
