053824fc9f0db8cf97218fd9a0fe777a1f555597
Joseph M <jmedina1985@hotmail.com>
2023-04-18 13:17:43 -0700

    Final adjustments!

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ cards.forEach((card, index) => {
     if (!isCardActive) card.classList.add('active');
 
     Flip.from(state, {
-      duration: 1.2,
+      duration: 1.3,
       ease: 'expo.out',
       absolute: true,
     });
